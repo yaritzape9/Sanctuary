1854c4dc52567081b78f2eae5a0a18daa317e5d0
yaritza perez <yaritzape9@gmail.com>
2017-02-24 19:08:38 -0800

    fixes root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  root to:'index#index'
+
   resources :users
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#delete', as: 'logout'
 
-  root 'index'
 
 end
