e361d911305c98d43f3ac54b0968f6b601bc0a88
JadziaHax <madelyn.a.thomas@gmail.com>
2017-02-26 21:50:44 -0800

    Lets user place new markers by clicking, Markers display info when clicked.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -18,13 +18,48 @@ function initMap() {
       dataType: 'json'
     });
 
+    function placeMarker(location) {
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+
+
+      var geocoder = new google.maps.Geocoder();
+      var infoWindow = new google.maps.InfoWindow();
+
+      var latLng = {lat: marker.getPosition().lat(), lng: marker.getPosition().lng()};
+
+      geocoder.geocode({'location': latLng}, function(results, status) {
+        if (status === 'OK') {
+          if (results[1]) {
+            infoWindow.setContent(results[1].formatted_address);
+          } else {
+            infoWindow.setContent(latLng.lat + ', ' + latLng.lng);
+          }
+        } else {
+          infoWindow.setContent(latLng.lat + ', ' + latLng.lng);
+        }
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(map, marker);
+      });
+    }
+
     request.done( function(response) {
       for (var i = 0; i < response.length; i++) {
-        var marker = new google.maps.Marker({
-          position: {lat: response[i].longitude, lng: response[i].latitude},
-          map: map
-        });
+        var latLngLiteral = {
+          lat: response[i].longitude,
+          lng: response[i].latitude
+        };
+
+        placeMarker(latLngLiteral);
       }
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+       placeMarker(event.latLng);
     });
   }
 }
