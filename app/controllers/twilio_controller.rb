db6cb8cb00093f7930eeba7308946fbadc526a50
yaritza perez <yaritzape9@gmail.com>
2017-03-01 15:54:31 -0800

    adds conditional to check if user has contacts

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,19 +5,20 @@ class TwilioController < ApplicationController
   def trigger_sms_alerts
     alert_message = "current_user.alert_msg"
     contacts = current_user.contacts
+    if contacts.empty?
+      flash[:alert] = "You have no contacts. Try adding contacts"
+    else
+        contacts.each do |contact|
+          begin
+            phone_number = contact.phone_num
+            send_message(phone_number, alert_message)
+            flash[:success] = "Messages sent."
+          rescue
+            flash[:alert] = 'Something when wrong.'
+          end
+        end
+    end
 
-    contacts.each do |contact|
-      begin
-        phone_number = contact.phone_num
-        send_message(phone_number, alert_message)
-        p "it works"
-        flash[:success] = "Messages sent."
-      rescue
-        send_message(phone_number, alert_message)
-        p "failure!"
-        flash[:alert] = 'Something when wrong.'
-      end
-  end
   redirect_to rights_path
 end
 private
